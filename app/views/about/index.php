d0764fa91bc7c5bc27b04854243dc4261e00a97f
Fahmi Bahtiar <fahmibahtiar76@gmail.com>
2021-01-06 10:36:31 +0700

    commit

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
+<div class="container">
 
-    <h1>About Me</h1>
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/pp.png" alt="Fahmi Bahtiar" width="200" class="rounded-circle shadow" >
     <p>Halo Nama Saya <?= $data['nama']; ?> Hobi Saya <?= $data['hobi']; ?> Saya Berumur <?= $data['umur']; ?></p>
+    
+</div>
